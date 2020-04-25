98de02a70413e2e32f98eab56e30be5038767686
elijah-atkins <elijahorama@gmail.com>
2020-04-25 17:19:41 -0600

    started project, simple form setup

---
+++ src/components/Form.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const TeamForm = ({ addNewTeam }) => {
+
+  const [team, setTeam] = useState({ title: "", body: "" });
+  // This uses https://reactjs.org/docs/events.html#form-events
+  const handleChanges = event => {
+
+    setTeam({ ...team, [event.target.name]: event.target.value });
+
+  };
+
+
+  const submitForm = event => {
+    event.preventDefault();
+    addNewTeam(team);
+    setTeam({ name: "", email: "", role: "" });
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+
+      <label htmlFor="name">Name</label>
+
+      <input
+        id="name"
+        type="text"
+        placeholder="Enter Name"
+        value={team.name}
+        name="name"
+        onChange={handleChanges}
+      />
+      <label htmlFor="email">E-mail</label>
+
+<input
+  id="email"
+  type="text"
+  placeholder="Enter E-mail"
+  value={team.email}
+  name="email"
+  onChange={handleChanges}
+/>      <label htmlFor="role">Role</label>
+
+<input
+  id="role"
+  type="text"
+  placeholder="Enter Role"
+  value={team.role}
+  name="role"
+  onChange={handleChanges}
+/>
+      <button>Add Team Member</button>
+    </form>
+  );
+};
+
+export default TeamForm;
---
+++ src/components/data.js
@@ -0,0 +1,6 @@
+export default [{
+    id: 1,
+    name: "Elijah Atkins",
+    email: "elijah-the-atkins@gmail.com",
+    role: "Web UI/React"
+}]
